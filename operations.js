a5b752d961e2c292114fec03e1f9b1fddabbed0c
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2020-11-21 20:54:02 -0700

    Fix stray quotes

--- operations.js
+++ operations.js
@@ -4,7 +4,7 @@ const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
 const validateNumbers = (x, y) => {
-  if (isNaN(x) ||`` isNaN(y)) {
+  if (isNaN(x) || isNaN(y)) {
     return false;
   }
   return true;
